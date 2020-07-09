ef814dd16afe00cf991cdd1eedcbc0f268f02888
Johan Vargas <johan.vargas@globant.com>
2020-07-09 10:21:39 -0500

    Initial setup added basic Spring Boot project.

---
+++ src/main/java/com/todopago/pocmultitenancy/PocMultitenancyApplication.java
@@ -0,0 +1,13 @@
+package com.todopago.pocmultitenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMultitenancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMultitenancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todopago/pocmultitenancy/PocMultitenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todopago.pocmultitenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMultitenancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
